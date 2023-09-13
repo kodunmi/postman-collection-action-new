c333ffd75e5781c5fd58cdbbf3d13ad7d6bba496
kodunmi <53086177+kodunmi@users.noreply.github.com>
2023-09-13 07:25:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+<p align="center">
+  <a href="https://github.com/jneate/postman-collection-action/actions"><img alt="postman-collection-action status" src="https://github.com/jneate/postman-collection-action/workflows/build-test/badge.svg"></a>
+</p>
+
+# Postman Collection Action
+
+This GitHub action will scan the repository contents for any Postman Collection JSON files and create or update the Collection in a specific [Postman Workspace](https://web.postman.co/workspace), the check to determine whether to create a new collection or update an existing one is based on the Collection ID, this is the `info._postman_id` field in an exported v2.1 collection.
+
+If the Postman API returns a different Collection ID then the action will update the file, if you wish to commit this back into the Git repository then you can combine this action with `stefanzweifel/git-auto-commit-action@v4` which automatically commits the changed file(s). See the example usage sections below for more information.
+
+>*Note: This action uses the `\t` character as formatting for the JSON document, the first commit might result in a large change due to this formatting character*
+
+The local search process finds any `.json` files, attempts to parse them as valid JSON objects and then checks the `info.schema` field to match `https://schema.getpostman.com/json/collection/v2.1.0/collection.json`.
+
+The following 4 Postman APIs are currently used:
+
+- Get All Collections - GET - `https://api.getpostman.com/collections`
+- Create a New Collection - POST - `https://api.getpostman.com/collections`
+- Create a New Collection in a Workspace - POST - `https://api.getpostman.com/collections?workspace={{workspace_id}}`
+- Update A Collections - `https://api.getpostman.com/collections/{{collection_uid}}`
+
+## Inputs
+
+### postmanApiKey
+
+**Required** This is the postman API key you have created that has access to your workspace.
+
+If you don't have an API Key, you can follow the instructions [here](https://learning.postman.com/docs/developer/intro-api/#generating-a-postman-api-key)
+
+> *It's recommended to store this value in a secret so it's not visible in any log output / file content*
+
+### postmanWorkspaceId
+
+**Optional** This is the ID of the Workspace the collection should be created/updated in. *Default*: `My Workspace` which is associated to your API Key.
+
+### postmanTimeout
+
+**Optional** This is the number of milliseconds to wait for the Postman APIs to respond before timing out. *Default*: `15000`
+
+## Example Usage
+
+### All Inputs
+
+```yaml
+- name: Checkout
+  uses: actions/checkout@v2
+
+- name: Sync Postman Collections
+  uses: jneate/postman-collection-action@v1
+  with:
+    postmanApiKey: ${{ secrets.postmanApiKey }}
+    postmanWorkspaceId: 0f41daa6-c9a7-49d9-8455-707e2f46da22
+    postmanTimeout: 30000
+```
+
+### With Auto Git Commit
+
+```yaml
+- name: Checkout
+  uses: actions/checkout@v2
+
+- name: Sync Postman Collections
+  uses: jneate/postman-collection-action@v1
+  with:
+    postmanApiKey: ${{ secrets.postmanApiKey }}
+    postmanWorkspaceId: 0f41daa6-c9a7-49d9-8455-707e2f46da22
+    postmanTimeout: 30000
+
+- name: Commit Updated Postman Collection IDs
+  uses: stefanzweifel/git-auto-commit-action@v4
+  with:
+    commit_message: Updated Postman Collection ID
+```
+
+### Required Only Inputs
+
+```yaml
+- name: Checkout
+  uses: actions/checkout@v2
+
+- name: Sync Postman Collections
+  uses: jneate/postman-collection-action@v1
+  with:
+    postmanApiKey: ${{ secrets.postmanApiKey }}
+```
---
+++ src/main.ts
@@ -0,0 +1,256 @@
+// import * as core from '@actions/core'
+// import * as glob from '@actions/glob'
+// import {promises} from 'fs'
+// import axios, {AxiosInstance, AxiosResponse} from 'axios'
+// import {
+//   LocalCollection,
+//   RemoteCollection,
+//   RemoteCollectionContainer
+// } from './types'
+
+// const localPostmanCollections: LocalCollection[] = []
+// const localPostmanCollectionFileMap: Map<string, string> = new Map()
+// const remotePostmanCollectionsMap: Map<string, RemoteCollection> = new Map()
+
+// const restClient: AxiosInstance = axios.create({
+//   baseURL: 'https://api.getpostman.com',
+//   timeout: Number(core.getInput('postmanTimeout')) || 15000,
+//   headers: {
+//     'X-Api-Key': core.getInput('postmanApiKey')
+//   }
+// })
+
+// const postmanWorkspaceId = core.getInput('postmanWorkspaceId')
+
+// async function run(): Promise<void> {
+//   try {
+//     await Promise.all([
+//       loadLocalPostmanCollections(),
+//       loadRemotePostmanCollections()
+//     ])
+
+//     if (localPostmanCollections.length === 0) {
+//       // No local postman collections found so exit early
+//       return
+//     }
+
+//     await Promise.all(
+//       localPostmanCollections.map(async (localCollection: LocalCollection) => {
+//         try {
+//           // const remoteCollection: RemoteCollection | undefined =
+//           //   remotePostmanCollectionsMap.get(localCollection.info._postman_id)
+
+//           let response: AxiosResponse<RemoteCollectionContainer>
+
+//           // Collection not found in Remote Workspace so send a POST Request to create the collection
+//           const createURi: string = postmanWorkspaceId
+//             ? `/collections?workspace=${postmanWorkspaceId}`
+//             : `/collections`
+//           response = await restClient.post(createURi, {
+//             collection: localCollection
+//           })
+//           if (
+//             localCollection.info._postman_id !== response.data.collection.id
+//           ) {
+//             // IDs are different, update local file
+//             const oldId: string = localCollection.info._postman_id
+//             const localPath: string | undefined =
+//               localPostmanCollectionFileMap.get(oldId)
+//             if (localPath) {
+//               localCollection.info._postman_id = response.data.collection.id
+//               await promises.writeFile(
+//                 localPath,
+//                 JSON.stringify(localCollection, null, '\t')
+//               )
+//             }
+//           }
+//           localCollection.info._postman_id = response.data.collection.id
+
+//           core.info(
+//             `Successfully created collection ${response.data?.collection?.name} with Postman ID ${response.data?.collection?.id}`
+//           )
+//         } catch (error) {
+//           core.error(
+//             `Status ${error.response?.status} - Unable to process collection ${localCollection.info.name} with Postman ID ${localCollection.info._postman_id} due to: ${error.response?.data?.error?.message}`
+//           )
+//           core.setFailed(
+//             `Errors processing Postman Collection(s) - Please see the output above`
+//           )
+//         }
+//       })
+//     )
+//   } catch (error) {
+//     core.setFailed(error.message)
+//   }
+// }
+
+// async function loadRemotePostmanCollections(): Promise<void> {
+//   try {
+//     const {data} = await restClient.get('/collections')
+//     for (const remoteCollection of data.collections.filter(
+//       (collection: RemoteCollection) => !collection.fork
+//     )) {
+//       remotePostmanCollectionsMap.set(remoteCollection.id, remoteCollection)
+//     }
+
+//     core.info(
+//       `${remotePostmanCollectionsMap.size} Non-Forked Collection(s) found for the given API Key in Remote Postman`
+//     )
+//   } catch (error) {
+//     core.setFailed(
+//       `Status ${error.response?.status} - Response: ${error.response?.data}`
+//     )
+//     throw new Error(`Unable to fetch Remote Collections from Postman Workspace`)
+//   }
+// }
+
+// async function loadLocalPostmanCollections(): Promise<void> {
+//   // Recursively search through the Repository files for JSON Files
+//   const jsonPattern = `**/*.json`
+//   const globber: glob.Globber = await glob.create(jsonPattern)
+//   const files: string[] = []
+
+//   for await (const file of globber.globGenerator()) {
+//     // Store the file name(s)
+//     files.push(file)
+//   }
+
+//   core.info(`${files.length} JSON File(s) Found`)
+
+//   if (files.length === 0) {
+//     return
+//   }
+
+//   // Wait for all files to be processed before progressing
+//   await Promise.all(
+//     files.map(async file => {
+//       // Read the file content in memory and convert to JSON
+//       try {
+//         const jsonContent = JSON.parse(
+//           (await promises.readFile(file)).toString()
+//         )
+//         // Check if the JSON file is a "valid" Postman v2.1 Collection, when true store in array
+//         if (
+//           jsonContent?.info?.schema ===
+//           `https://schema.getpostman.com/json/collection/v2.1.0/collection.json`
+//         ) {
+//           localPostmanCollections.push(jsonContent)
+//           localPostmanCollectionFileMap.set(jsonContent.info._postman_id, file)
+//         }
+//       } catch (e) {
+//         // If JSON can't be parsed it's not valid so ignore
+//       }
+//     })
+//   )
+
+//   core.info(
+//     `${localPostmanCollections.length} JSON Postman Collection(s) found`
+//   )
+// }
+
+// run()
+
+import * as core from '@actions/core'
+import * as glob from '@actions/glob'
+import {promises} from 'fs'
+import axios, {AxiosInstance} from 'axios'
+// import {AxiosInstance} from 'axios'
+import {LocalCollection} from './types'
+
+const localPostmanCollections: LocalCollection[] = []
+const localPostmanCollectionFileMap: Map<string, string> = new Map()
+
+const restClient: AxiosInstance = axios.create({
+  baseURL: 'https://api.getpostman.com',
+  timeout: Number(core.getInput('postmanTimeout')) || 15000,
+  headers: {
+    'X-Api-Key': core.getInput('postmanApiKey')
+  }
+})
+
+async function run(): Promise<void> {
+  try {
+    await loadLocalPostmanCollections('storage/app/postman')
+
+    if (localPostmanCollections.length === 0) {
+      // No local postman collections found so exit early
+      return
+    }
+
+    await Promise.all(
+      localPostmanCollections.map(async (localCollection: LocalCollection) => {
+        try {
+          // Send a POST Request to create the collection
+          const createURi: string = core.getInput('postmanWorkspaceId')
+            ? `/collections?workspace=${core.getInput('postmanWorkspaceId')}`
+            : `/collections`
+          const response = await restClient.post(createURi, {
+            collection: localCollection
+          })
+
+          core.info(
+            `Successfully created collection ${response.data?.collection?.name} with Postman ID ${response.data?.collection?.id}`
+          )
+        } catch (error) {
+          core.error(
+            `Error creating collection: ${error.response?.status} - ${error.response?.data?.error?.message}`
+          )
+          core.setFailed(
+            `Errors processing Postman Collection(s) - Please see the output above`
+          )
+        }
+      })
+    )
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+async function loadLocalPostmanCollections(folder: string): Promise<void> {
+  // Search for JSON files in the specified folder
+  const jsonPattern = `${folder}/**/*.json`
+  const globber: glob.Globber = await glob.create(jsonPattern)
+  const files: string[] = []
+
+  for await (const file of globber.globGenerator()) {
+    // Store the file name(s)
+    files.push(file)
+  }
+
+  core.info(`${files.length} JSON File(s) Found in ${folder}`)
+
+  if (files.length === 0) {
+    return
+  }
+
+  // Wait for all files to be processed before progressing
+  await Promise.all(
+    files.map(async file => {
+      // Read the file content in memory and convert to JSON
+      try {
+        const jsonContent = JSON.parse(
+          (await promises.readFile(file)).toString()
+        )
+        // Check if the JSON file is a "valid" Postman v2.1 Collection, when true store in array
+        if (
+          jsonContent?.info?.schema ===
+          `https://schema.getpostman.com/json/collection/v2.1.0/collection.json`
+        ) {
+          localPostmanCollections.push(jsonContent)
+          localPostmanCollectionFileMap.set(jsonContent.info._postman_id, file)
+        }
+      } catch (e) {
+        // If JSON can't be parsed, it's not valid, so ignore
+      }
+    })
+  )
+
+  core.info(
+    `${localPostmanCollections.length} JSON Postman Collection(s) found in ${folder}`
+  )
+  core.info(
+    `${localPostmanCollections.length} JSON Postman Collection(s) found in ${folder}`
+  )
+}
+
+run()
